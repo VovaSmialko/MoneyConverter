0ec94a0bb2d98e87c75a33db0db1c52022042721
vova77 <vova2000smi@gmail.com>
2025-03-24 20:33:23 +0200

    created component for the application

--- app/src/main/java/com/smialko/moneyconverter/MainActivity.kt
+++ app/src/main/java/com/smialko/moneyconverter/MainActivity.kt
@@ -11,11 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
+import com.mc.designsystem.components.MCTextMenu
+import com.mc.designsystem.theme.MoneyConverterTheme
 import com.mc.network.service.CurrencyService
-import com.smialko.moneyconverter.ui.theme.MoneyConverterTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -26,38 +25,11 @@ class MainActivity : ComponentActivity() {
     lateinit var service: CurrencyService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        lifecycleScope.launch {
-            service.getExchangeRates().also {
-                println("test12345: $it")
-            }
-        }
-        enableEdgeToEdge()
         setContent {
             MoneyConverterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MCTextMenu(selectedOption = "USD", options = listOf("USD", "EUR", "GBT")) {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MoneyConverterTheme {
-        Greeting("Android")
     }
 }
--- core/designssystem/src/main/kotlin/com/mc/designsystem/theme/Theme.kt
+++ core/designssystem/src/main/kotlin/com/mc/designsystem/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.smialko.moneyconverter.ui.theme
+package com.mc.designsystem.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
